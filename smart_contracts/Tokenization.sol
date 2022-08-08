d118e21bf61fd0122e46f2612fc23ad8c03cafcf
Gabriel Rigoli <gabrielr.akio@gmail.com>
2022-08-07 21:47:30 -0300

    Add transfer function and fix some issues

--- smart_contracts/Tokenization.sol
+++ smart_contracts/Tokenization.sol
@@ -1,30 +1,31 @@
 /* SPDX-License-Identifier: UNLICENSED */
 pragma solidity ^0.8.7;
 
-contract Tokenization {
+contract AssetToken {
 
     // Define collateral structure
 
     struct Collateral {
         address bankId;
-        uint collateralQuantity;
+        uint collateralShares;
         uint expirationDate;
     }
 
     // Define owner structure
 
     struct Owner {
-        address ownerId;
-        uint ownerQuantity;
+        uint shares;
         Collateral[] collaterals;
     }
 
     // Define state variables for the contract
 
-    address public creator;                     // contract creator: must be platform. For auditability purposes
-    address public effectiveOwner;              // address of the property's current owner
+    address public creator = msg.sender;                     // contract creator: must be platform. For auditability purposes
+    address public effectiveOwner;                           // address of the property's current owner  
+    address[] public owners;                                 // array with addresses of owners
+    mapping (address => Owner) public percentageOwners;     // mapping all owners               
+                
     uint public totalSupply;                    // total supply of tokens. Default set to 1000, so 1% of the asset equal 10 tokens
-    Owner[] public percentageOwners;            // information about percentage owners of the asset, including eventual collaterals associated with them
     string public assetAddress;                 // address of the asset being tokenized
     uint public assetUsableArea;                // usable area of the tokenized asset
     uint public assetId;                        // asset identification number (In Brazil, Cadastro Imobiliário Brasileiro (CIB) (???) )
@@ -36,39 +37,106 @@ contract Tokenization {
 	    _;
     }
 
-    modifier onlyOwner {
+    modifier onlyEffectiveOwner {
 	    require(msg.sender == effectiveOwner);
 	    _;
 	}
 
-    modifier onlyPlatformOrOwner {
+    modifier onlyOwner {
+        (bool isOwner, ) = isAddressOwner(msg.sender);
+	    require(isOwner);
+	    _;
+	}
+
+    modifier onlyPlatformOrEffectiveOwner {
         require(msg.sender == creator || msg.sender == effectiveOwner);
+	    _;
+    }
+
+    modifier onlyPlatformOrOwner {
+        (bool isOwner, ) = isAddressOwner(msg.sender);
+        require(msg.sender == creator || isOwner);
 	    _;
     }
 
     // Define constructor to create tokenization
 
-    constructor(address _ownerId, string memory _assetAddress, uint _assetUsableArea, uint _assetId) {
+    constructor(address _effectiveOwner, string memory _assetAddress, uint _assetUsableArea, uint _assetId) {
         creator = msg.sender;
         assetAddress = _assetAddress;
         assetUsableArea = _assetUsableArea;
         assetId = _assetId;
-        effectiveOwner = _ownerId;
+        effectiveOwner = _effectiveOwner;
         totalSupply = 1000;
-        Owner storage o = percentageOwners.push();
-        o.ownerId = _ownerId;
-        o.ownerQuantity = 1000;
+        percentageOwners[_effectiveOwner].shares = totalSupply;
+
+        owners.push(effectiveOwner);
+        owners.push(creator);
+    }
+
+    function isAddressOwner(address _address) public view returns(bool, uint256) {
+        for (uint256 i = 0; i < owners.length; i += 1) {
+	        if (_address == owners[i]) return (true, i);
+	    }
+	    return (false, 0); 
     }
 
     // Function to get number of owners of the asset
 
-    function getOwnerNumber() public view onlyPlatformOrOwner returns(uint count) {
-        return percentageOwners.length;
+    function getOwnersNumber() public view returns(uint) {
+        return owners.length;
     }
 
     // Function to get number of collaterals associated with owner
 
-    function getCollateralNumber(uint _ownerId) public view onlyPlatformOrOwner returns(uint count) {
-        return percentageOwners[_ownerId].collaterals.length;
+    function getCollateralNumber(address _owner) public view onlyPlatformOrOwner returns(uint) {
+        return percentageOwners[_owner].collaterals.length;
+    }
+
+    // Get shares as collaterals by owner
+
+    function getCollateralShares(address _owner) public view onlyPlatformOrOwner returns(uint) {
+        Collateral[] storage collaterals =  percentageOwners[_owner].collaterals;
+        uint256 sum = 0;
+
+        for(uint256 i = 0; i < collaterals.length; i++)
+            sum = sum + collaterals[i].collateralShares;
+
+        return sum;
+    }
+    
+    // Function to remove owner from owners array
+
+    function removeOwner(address _owner) public onlyPlatform 
+    {
+        (bool isOwner, uint256 index) = isAddressOwner(_owner);
+
+        if (!isOwner || _owner == effectiveOwner) return; // dont delete when address isnt on array or when owner is effective owner
+
+        owners[index] = owners[owners.length - 1];
+        owners.pop();
+    }
+
+    // Function to split ownership between owners
+
+    function transferOwnership(uint _transferPercentage, address _buyer, bool _isEffectiveTransfer) public onlyPlatformOrOwner 
+    {
+
+        Owner storage payer = percentageOwners[msg.sender];
+        require((payer.shares - getCollateralShares(msg.sender)) >= _transferPercentage);
+
+        if (_isEffectiveTransfer) {
+            require(msg.sender == effectiveOwner);
+            effectiveOwner = _buyer;
+        }
+
+        payer.shares -= _transferPercentage;
+        percentageOwners[_buyer].shares += _transferPercentage;
+
+        (bool isBuyerOwner,) =  isAddressOwner(_buyer);
+
+        if (!isBuyerOwner) owners.push(_buyer);
+
+        if (payer.shares == 0) removeOwner(msg.sender);             
     }
 }
